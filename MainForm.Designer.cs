85c844419979cac2f6f732413f94ce7d9a3cae2a
Martin Kaul <martin@familie-kaul.de>
2020-02-19 11:17:32 +0100

    Commit of first version - included feature is sending spaces to an selected application when voice is higher than the level.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace SendVoiceCommands
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._closeButton = new System.Windows.Forms.Button();
+            this._processesBox = new System.Windows.Forms.ComboBox();
+            this._refreshButton = new System.Windows.Forms.Button();
+            this._testButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _closeButton
+            // 
+            this._closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._closeButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this._closeButton.Location = new System.Drawing.Point(713, 415);
+            this._closeButton.Name = "_closeButton";
+            this._closeButton.Size = new System.Drawing.Size(75, 23);
+            this._closeButton.TabIndex = 0;
+            this._closeButton.Text = "_closeButton";
+            this._closeButton.UseVisualStyleBackColor = true;
+            this._closeButton.Click += new System.EventHandler(this.closeButton__Click);
+            // 
+            // _processesBox
+            // 
+            this._processesBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._processesBox.FormattingEnabled = true;
+            this._processesBox.Location = new System.Drawing.Point(12, 12);
+            this._processesBox.Name = "_processesBox";
+            this._processesBox.Size = new System.Drawing.Size(695, 21);
+            this._processesBox.TabIndex = 1;
+            this._processesBox.SelectedIndexChanged += new System.EventHandler(this._processesBox_SelectedIndexChanged);
+            // 
+            // _refreshButton
+            // 
+            this._refreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._refreshButton.Location = new System.Drawing.Point(713, 12);
+            this._refreshButton.Name = "_refreshButton";
+            this._refreshButton.Size = new System.Drawing.Size(75, 23);
+            this._refreshButton.TabIndex = 2;
+            this._refreshButton.Text = "_refreshButton";
+            this._refreshButton.UseVisualStyleBackColor = true;
+            this._refreshButton.Click += new System.EventHandler(this._refreshButton_Click);
+            // 
+            // _testButton
+            // 
+            this._testButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this._testButton.Location = new System.Drawing.Point(632, 415);
+            this._testButton.Name = "_testButton";
+            this._testButton.Size = new System.Drawing.Size(75, 23);
+            this._testButton.TabIndex = 3;
+            this._testButton.Text = "_testButton";
+            this._testButton.UseVisualStyleBackColor = true;
+            this._testButton.Click += new System.EventHandler(this._testButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 129);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 78);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(776, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 50;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this._closeButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this._testButton);
+            this.Controls.Add(this._refreshButton);
+            this.Controls.Add(this._processesBox);
+            this.Controls.Add(this._closeButton);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _closeButton;
+        private System.Windows.Forms.ComboBox _processesBox;
+        private System.Windows.Forms.Button _refreshButton;
+        private System.Windows.Forms.Button _testButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendVoiceCommands
+{
+    public partial class MainForm : Form
+    {
+        class ProcessItem
+        {
+            public string title;
+            public System.Diagnostics.Process process;
+
+            override public string ToString()
+            {
+                return title;
+            }
+        };
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private NAudio.Wave.WaveInEvent waveIn;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Text = "Send Voice Command";
+            _closeButton.Text = "Close";
+            _refreshButton.Text = "Refresh";
+            _testButton.Text = "Test";
+
+            refreshProcessList();
+
+            waveIn = new NAudio.Wave.WaveInEvent();
+            waveIn.DataAvailable += OnDataAvailable;
+            waveIn.StartRecording();
+        }
+
+        private void closeButton__Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void refreshProcessList()
+        {
+            _processesBox.Items.Clear();
+            _testButton.Enabled = false;
+            _processesBox.Text = "";
+
+            System.Diagnostics.Process[] processList = System.Diagnostics.Process.GetProcesses();
+            Array.Sort(processList, (x, y) => String.Compare(x.ProcessName, y.ProcessName));
+
+            foreach (System.Diagnostics.Process process in processList)
+            {
+                if (process.MainWindowHandle == null) continue;
+                if (process.MainWindowHandle.ToInt32() == 0x00000000) continue;
+
+                ProcessItem item = new ProcessItem();
+                item.process = process;
+                item.title = process.ProcessName + " [" + process.Id + "]";
+
+                _processesBox.Items.Add(item);
+            }
+        }
+
+        private void _testButton_Click(object sender, EventArgs e)
+        {
+            trigger();
+        }
+
+        private void _processesBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (_processesBox.SelectedItem != null)
+            {
+                _testButton.Enabled = true;
+            }
+            else
+            {
+                _testButton.Enabled = false;
+            }
+        }
+
+        private void _refreshButton_Click(object sender, EventArgs e)
+        {
+            refreshProcessList();
+        }
+
+        private void OnDataAvailable(object sender, NAudio.Wave.WaveInEventArgs args)
+        {
+            float max = 0;
+            // interpret as 16 bit audio
+            for (int index = 0; index < args.BytesRecorded; index += 2)
+            {
+                short sample = (short)((args.Buffer[index + 1] << 8) |
+                                        args.Buffer[index + 0]);
+                // to floating point
+                var sample32 = sample / 32768f;
+                // absolute value 
+                if (sample32 < 0) sample32 = -sample32;
+                // is this the max value?
+                if (sample32 > max) max = sample32;
+            }
+
+            int percentValue = (int)(100 * max);
+            MethodInvoker mi = new MethodInvoker(() => calculateMaximum(percentValue));
+            if (progressBar1.InvokeRequired)
+            {
+                progressBar1.Invoke(mi);
+            }
+            else
+            {
+                mi.Invoke();
+            }
+
+        }
+
+        private void calculateMaximum(int percentValue)
+        {
+            progressBar1.Value = percentValue;
+            if (percentValue >= trackBar1.Value)
+            {
+                trigger();
+            }
+        }
+
+        private void trigger()
+        {
+            ProcessItem item = _processesBox.SelectedItem as ProcessItem;
+            if (item == null)
+            {
+                return;
+            }
+
+            SetForegroundWindow(item.process.MainWindowHandle);
+            SendKeys.SendWait(" ");
+            SendKeys.Flush();
+        }
+    }
+}
